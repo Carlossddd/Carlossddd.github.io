3aed8081acb5319a6210159e5bbeb43d58697cb7
Carlossddd <carloservin2011@gmail.com>
2025-11-07 18:25:38 -0400

    Update index_js.js

--- index_js.js
+++ index_js.js
@@ -1,6 +1,3 @@
-// Simple welcome message
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Welcome to the One Piece fanpage!");
-
-    alert("☠️ Welcome to the One Piece Fanpage! 🌊");
-});
+function showMessage() {
+    alert("Welcome to One Piece!");
+}
